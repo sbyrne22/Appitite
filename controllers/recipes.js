82302f79d28bebc9a7aaa680c0f075204fac4b14
Sean Byrne <spickles@Seans-iMac.local>
2017-12-04 17:25:16 -0500

    Can now create a recipe and it will display on the recipe index page

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -8,8 +8,8 @@ const Recipe = require('../models/recipes.js');
 // index route
 router.get('/', async (req, res) => {
   const username = req.body.username;
-  const allrecipes = await Recipe.find();
-  res.render('recipes/index.ejs', {username, allrecipes});
+  const allRecipes = await Recipe.find();
+  res.render('recipes/index.ejs', {username, allRecipes});
   // if (req.session.logged) {
   //   res.render('recipes/index.ejs', {
   //     recipe: allrecipes,
@@ -20,29 +20,30 @@ router.get('/', async (req, res) => {
   // };
 });
 
-// show route
-router.get('/:id', async (req, res) => {
-  const oneRecipe = await Recipe.findById(req.params.id);
-  // const user = await Comment.find({ photo: onePhoto._id });
-
-  res.render('recipes/show.ejs', {
-    oneRecipe: oneRecipe,
-    // comments: comments
- });
-});
-
-// create route
 router.get('/new', async (req, res) => {
   res.render('recipes/new.ejs');
 });
 
 router.post('/new', async (req, res) => {
   try {
-    const createdRecipe = await Recipe.create(req.body);
+    const newRecipe = await Recipe.create(req.body);
     res.redirect('/');
   } catch (err) {
     res.send(err.message);
   }
 });
+
+// show route
+router.get('/:id', async (req, res) => {
+  const oneRecipe = await Recipe.findById(req.params.id);
+  // const user = await Comment.find({ photo: onePhoto._id });
+
+  res.render('recipes/show.ejs', {
+    oneRecipe: oneRecipe
+ });
+});
+
+// create route
+
 
 module.exports = router;
